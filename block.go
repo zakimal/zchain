c6ecb6d155d2fa79255f1add1e6eb3616cd73997
Ozaki Yoichi <zak@inl.ics.keio.ac.jp>
2021-05-21 20:39:57 +0900

    part3: boltDB, CLI

--- block.go
+++ block.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"bytes"
+	"encoding/gob"
+	"log"
 	"time"
 )
 
@@ -24,4 +27,29 @@ func NewBlock(data string, prevBlockHash []byte) *Block {
 
 func NewGenesisBlock() *Block {
 	return NewBlock("Genesis Block", []byte{})
+}
+
+func (b *Block) Serialize() []byte {
+	var result bytes.Buffer
+	encoder := gob.NewEncoder(&result)
+
+	err := encoder.Encode(b)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return result.Bytes()
+}
+
+func DeserializedBlock(d []byte) *Block {
+	var block Block
+
+	decoder := gob.NewDecoder(bytes.NewReader(d))
+
+	err := decoder.Decode(&block)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return &block
 }
--- main.go
+++ main.go
@@ -1,22 +1,10 @@
 package main
 
-import (
-	"fmt"
-	"strconv"
-)
-
 func main() {
 	bc := NewBlockchain()
 
-	bc.AddBlock("Send 1 BTC to Alice")
-	bc.AddBlock("Send 2 more BTC to Alice")
+	defer bc.db.Close()
 
-	for _, block := range bc.blocks {
-		fmt.Printf("Prev. hash: %x\n", block.PrevBlockHash)
-		fmt.Printf("Data: %s\n", block.Data)
-		fmt.Printf("Hash: %x\n", block.Hash)
-		pow := NewPoW(block)
-		fmt.Printf("PoW: %s\n", strconv.FormatBool(pow.Validate()))
-		fmt.Println()
-	}
+	cli := CLI{bc}
+	cli.Run()
 }
